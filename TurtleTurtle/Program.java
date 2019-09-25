d4500e92fdabe3182617b1ddf5d31c3c4a8a5987
Marie Chrisman <chrismane21@asmsa.org>
2019-09-25 16:21:50 -0500

    removeLoopAndEditedCodeToWork

--- TurtleTurtle/Program.java
+++ TurtleTurtle/Program.java
@@ -3,32 +3,39 @@
  * This program asks the user for how many sides they would want in a polygon
  * Then the program prints the image
  */
-import java.util.Scanner;
+import java.util.Scanner;//for user input of sides
 
 public class Program
 {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello! This program draws polygons. Enter an integer number of sides for the polygon. Warning: big numbers take longer and may not fit the screen.");
-		int PureCircle = 360;//This makes the program make the right shape and when to stop
-		int Sides;//User's request
-		int Angle;//How much bob turns
+		
+		double PureCircle = 360;//This makes the program make the right shape and when to stop
+		double Sides;//User's request
+	    double Angle;//How much bob turns
+	    
 		Scanner sides = new Scanner(System.in);
 		Sides = sides.nextInt();
+		
 		Angle = PureCircle / Sides;
 		
 		Turtle bob;
 		bob = new Turtle();
-			
-		while(PureCircle > 0)
-		{
-			bob.speed(100);
-			bob.forward(50);
-			bob.left(Angle);
-			PureCircle = PureCircle - Angle;
+		moveTurtle(PureCircle, Sides, Angle, bob);
+	}
+	
+	public static void moveTurtle(double PureCircle,double Sides,double Angle,Turtle bob){
+		bob.forward(30);//Controls bob
+		bob.left(Angle);
+		
+		PureCircle = PureCircle - Angle;//Math that controls how much bob turns to complete the shape
+		Sides--;
+		
+		if(Sides > 0){//This is a extremation to stop bob from going continuously going
+			moveTurtle(PureCircle, Sides, Angle, bob);
 		}
-		
-		
-		
 	}
 }
+	
+
